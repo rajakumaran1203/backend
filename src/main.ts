991f27b90d021776ff4a9bc1875a64ae9b4a7c3e
rajakumaran1203 <44520800+rajakumaran1203@users.noreply.github.com>
2023-12-21 22:03:33 +0530

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 // import { ScheduleService } from './schedule.service';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const logger = new Logger('bootstarp');
@@ -11,16 +10,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-  //   origin: 'https://master--friendly-halva-89d6cc.netlify.app/',
-  //   credentials: true,
-  // });
-
-  const allowedOrigins = ['https://master--friendly-halva-89d6cc.netlify.app'];
-      app.use(cors({
-        origin: allowedOrigins,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-      }));
+    origin: 'https://master--friendly-halva-89d6cc.netlify.app/',
+    credentials: true,
+  });
+  
   const port = 3000;
   // const scheduleService = app.get(ScheduleService);
   // scheduleService.scheduleEmailTask();
