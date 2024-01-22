b3674bb3b8c4f48d7dbe0c68aa101eff97c54ba7
rajakumaran1203 <44520800+rajakumaran1203@users.noreply.github.com>
2024-01-22 21:05:03 +0530

    Cross origin

--- src/src/main.ts
+++ src/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const logger = new Logger('bootstarp');
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     credentials: true,
   });
   const port = 3000;
