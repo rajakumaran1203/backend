921fdb466d09e530baa6b6a3d23a2c30e7c98554
rajakumaran1203 <44520800+rajakumaran1203@users.noreply.github.com>
2024-02-08 10:56:22 +0530

    main js file update

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,40 @@
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { Logger } from '@nestjs/common';
+// // import { ScheduleService } from './schedule.service';
+
+// async function bootstrap() {
+//   const logger = new Logger('bootstarp');
+//   const app = await NestFactory.create(AppModule);
+
+//   app.useGlobalPipes(new ValidationPipe());
+//   app.enableCors({
+//     origin: '*',
+//     credentials: true,
+//   });
+//   const port = 3000;
+//   // const scheduleService = app.get(ScheduleService);
+//   // scheduleService.scheduleEmailTask();
+//   await app.listen(port);
+//   logger.log(`Application listening on port ${port}`);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-// import { ScheduleService } from './schedule.service';
 
 async function bootstrap() {
   const logger = new Logger('bootstarp');
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: '*',
     credentials: true,
   });
   const port = 3000;
-  // const scheduleService = app.get(ScheduleService);
-  // scheduleService.scheduleEmailTask();
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
-
-
 
